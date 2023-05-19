b0d1e1651855f2992bb9edd18914f5c8394d2c22
Mayur-1999 <102725772+Mayur-1999@users.noreply.github.com>
2023-05-20 00:31:34 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,13 +1,12 @@
 terraform {
   required_providers {
     google = {
-      source  = "hashicorp/google"
-      version = "~> 4.0"
+      source = "hashicorp/google"
+      version = "4.65.2"
     }
   }
 }
-provider "google" {
-  project = "var.project_id"
-  region  = "us-central1"
 
+provider "google" {
+  # Configuration options
 }
