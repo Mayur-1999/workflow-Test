d1399c7d5c5fa533468f742cee28134c5380296d
Mayur-1999 <102725772+Mayur-1999@users.noreply.github.com>
2023-05-20 00:33:15 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,12 +1,14 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
-      version = "4.65.2"
+      source  = "hashicorp/google"
+      version = "~> 4.0"
     }
   }
 }
-
 provider "google" {
-  # Configuration options
+  #credentials = "GCP_SA_KEY"
+  project = "pqwiklabs-gcp-02-3b220788b71a"
+  region  = "us-central1"
+
 }
