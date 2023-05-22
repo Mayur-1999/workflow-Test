4d340b27bda3baea481befab4696f6d6aa6f42c2
Mayur-1999 <102725772+Mayur-1999@users.noreply.github.com>
2023-05-22 13:09:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,3 +7,8 @@ resource "google_monitoring_dashboard" "gke_cluster_dashboard" {
   project        = var.project_id
   dashboard_json = file(var.gke_cluster_dashboard_json_filename)
 }
+
+resource "google_monitoring_dashboard" "spanner_dashboard" {
+  project        = var.project_id
+  dashboard_json = file(var.spanner_dashboard_json_filename)
+}
