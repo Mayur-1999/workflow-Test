117df7d4e51096da3dc4bf3df348321c20f68392
Mayur-1999 <102725772+Mayur-1999@users.noreply.github.com>
2023-05-22 15:47:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,15 +4,15 @@
 #  service = "monitoring.googleapis.com"
 #}
 
-resource "google_monitoring_dashboard" "gke_pod_dashboard" {
-  project        = var.project_id
-  dashboard_json = file(var.gke_pod_dashboard_json_filename)
-}
+#resource "google_monitoring_dashboard" "gke_pod_dashboard" {
+#  project        = var.project_id
+#  dashboard_json = file(var.gke_pod_dashboard_json_filename)
+#}
 
-resource "google_monitoring_dashboard" "gke_cluster_dashboard" {
-  project        = var.project_id
-  dashboard_json = file(var.gke_cluster_dashboard_json_filename)
-}
+#resource "google_monitoring_dashboard" "gke_cluster_dashboard" {
+#  project        = var.project_id
+#  dashboard_json = file(var.gke_cluster_dashboard_json_filename)
+#}
 
 resource "google_monitoring_dashboard" "spanner_dashboard" {
   project        = var.project_id
