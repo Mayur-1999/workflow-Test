54294fc38d30ba6ceef221d7a2c932ba24f19123
Mayur-1999 <102725772+Mayur-1999@users.noreply.github.com>
2023-05-24 11:05:02 +0530

    Delete dashboard_v1 directory

--- dashboard_v1/dashboards/main.tf
+++
@@ -1,4 +0,0 @@
-resource "google_monitoring_dashboard" "monitoring_dashboard" {
-  project        = var.project_id
-  dashboard_json = var.dashboard_json_filename
-}
--- dashboard_v1/dashboards/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "dashboard_json_filename" {
-  
-}
-
-variable "project_id" {
-  description = "The ID of the project in which the dashboard will be created."
-  type        = string
-  default     = "qwiklabs-gcp-04-bd981ead41a8"
-}
