96561faa0e88bd99fa2cf052518ec8ec5e9ca69c
Mayur-1999 <102725772+Mayur-1999@users.noreply.github.com>
2023-05-24 11:13:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,49 @@
-
-#resource "google_project_service" "monitoring" {
-#  project = var.project_id
-#  service = "monitoring.googleapis.com"
-#}
-
-resource "google_monitoring_dashboard" "gke_pod_dashboard" {
-  project        = var.project_id
-  dashboard_json = file(var.gke_pod_dashboard_json_filename)
+module "gke_pod_dashboard" {
+  source                  = "./dashboards"
+  dashboard_json_filename = file(var.gke_pod_dashboard_json_filename)
+  #project_id              = var.project_id
 }
 
-resource "google_monitoring_dashboard" "gke_cluster_dashboard" {
-  project        = var.project_id
-  dashboard_json = file(var.gke_cluster_dashboard_json_filename)
+module "gke_cluster_dashboard" {
+  source                  = "./dashboards"
+  dashboard_json_filename = file(var.gke_cluster_dashboard_json_filename)
+  #project_id              = var.project_id
 }
 
-resource "google_monitoring_dashboard" "spanner_dashboard" {
-  project        = var.project_id
-  dashboard_json = file(var.spanner_dashboard_json_filename)
+module "spanner_dashboard" {
+  source                  = "./dashboards"
+  dashboard_json_filename = file(var.spanner_dashboard_json_filename)
+  #project_id              = var.project_id
+}
+  
+module "notification-channel" {
+  source                  = "./modules/notification-channel"
+  #project_id              = var.project_id
+}
+
+module "gke" {
+  source                  = "./modules/gke"
+  CLUSTER_NAME            = "sre"
+  COE_team = module.notification-channel
+  CLUSTER_LOCATION            = "us-central1"
+  #project_id              = var.project_id
+}
+
+module "vm" {
+  source                  = "./modules/VM"
+  COE_team = module.notification-channel
+  #project_id              = var.project_id
+}
+
+module "spanner" {
+  source                  = "./modules/spanner"
+  COE_team = module.notification-channel
+  #SPANNER_NAME            = ""
+  #project_id              = var.project_id
+}
+
+module "prometheus" {
+  source                  = "./modules/prometheus-alert"
+  COE_team = module.notification-channel
+  #project_id              = var.project_id
 }
